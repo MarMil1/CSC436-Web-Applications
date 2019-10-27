1ae8c4481404625cebda2ab67270543273d9dffb
Marko Milosavljevic <marko@markos-air.attlocal.net>
2019-10-27 15:46:56 -0500

    Created todo-app and its components.

---
+++ Homework 4/todo-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { NewTodoItemComponent } from './new-todo-item/new-todo-item.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    NewTodoItemComponent,
+    PageNotFoundComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
