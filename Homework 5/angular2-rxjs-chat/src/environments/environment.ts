efd432d15de95140bd4528ea61d138d3846cc231
Marko Milosavljevic <marko@markos-air.attlocal.net>
2019-11-03 18:41:42 -0600

    Upload homework 5

---
+++ Homework 5/angular2-rxjs-chat/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD0n-GwHmgLOOoxBtyy33HYgJ6GywmCxTI",
+    authDomain: "chat-app-1f9b5.firebaseapp.com",
+    databaseURL: "https://chat-app-1f9b5.firebaseio.com",
+    projectId: "chat-app-1f9b5",
+    storageBucket: "chat-app-1f9b5.appspot.com",
+    messagingSenderId: "749475230762",
+    appId: "1:749475230762:web:6b4018dceffb05740aff53",
+    measurementId: "G-XS1K11BBE8"
+  }
+};
